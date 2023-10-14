e3eb7e3e258b37c3da55d7ec5ff6bc36283c189e
Jatin kumar <109894649+Jatin-Kumar-Thakur@users.noreply.github.com>
2023-10-14 13:13:15 +0530

    Add files via upload

---
+++ JavaScript-Learn/functions_jatinkumar.js
@@ -0,0 +1,41 @@
+// Author : Jatin Kumar
+// JavaScript Concept : Functions in JS
+// GITHUB : github.com/Jatin-Kumar-Thakur
+
+// function is a block of code that is used to perform a particular task
+// and funtions are useful in those cases in which we have to reuse the same block of code
+// again and again
+
+
+//So in javascript we can declare a function by using a key word function followed by the name 
+// of the function and round brackets which can have any number of parameters
+
+//Syntax of Js Function
+
+// function function_name(parameter1 ,parameter 2 ,......., parameter n){
+//     //code to execute
+// }
+
+
+// for an example 
+// sum of two number a and b
+//------------------------------------------------------------------------
+function sum(a , b){
+    return a+b;
+}
+
+console.log(sum(1,3));  //returns 4;
+
+
+//------------------------------------------------------------------
+//function  INvoke
+//we can simply call the function by its name and parameters inside brackets
+
+// like
+sum(1,4);
+
+
+//this is just a simple introduction of function in JS 
+//But in JS functions java lots of things that we have to learn so keep learning these thing too
+
+//thank you 
