a0a38363c3451fccbc45654a01f9fb3f7de38f3e
Jitisha Khede <khedejitisha2004@gmail.com>
2023-10-24 10:42:00 +0530

    properly described new math mathods in es6

--- JavaScript-Learn/ES6_CONCEPTS_shashankpantiitbh.js
+++ JavaScript-Learn/ES6_CONCEPTS_shashankpantiitbh.js
@@ -138,12 +138,7 @@ let first = numbers.findIndex(myFunction);
 function myFunction(value, index, array) {
   return value > 18;
 }
-// New Math Methods
-Math.trunc()
-Math.sign()
-Math.cbrt()
-Math.log2()
-Math.log10()
+
 
 // New Number Properties
 EPSILON
@@ -166,3 +161,34 @@ for (let x of f) {
 //Modules are imported in two differen ways:
 import { name, age } from "./person.js";
 import message from "./message.js";
+
+
+
+//New Math Methods
+/*ES6 added the following methods to the Math object:
+
+Math.trunc()
+Math.sign()
+Math.cbrt()
+Math.log2()
+Math.log10()
+*/
+
+//Math.trunc(x) returns the integer part of x
+Math.trunc(4.9);    // returns 4
+Math.trunc(4.7);    // returns 4
+Math.trunc(4.4);    // returns 4
+Math.trunc(4.2);    // returns 4
+Math.trunc(-4.2);    // returns -4
+//Math.sign(x) returns if x is negative, null or positive
+Math.sign(-4);    // returns -1
+Math.sign(0);    // returns 0
+Math.sign(4);    // returns 1
+//Math.cbrt(x) returns the cube root of x
+Math.cbrt(8);    // returns 2
+Math.cbrt(64);    // returns 4
+Math.cbrt(125);    // returns 5
+//Math.log2(x) returns the base 2 logarithm of x
+Math.log2(2);    // returns 1
+//Math.log10(x) returns the base 10 logarithm of x
+Math.log10(10);    // returns 1
